28554fe8ba4bb70efca731a5d86026989a938e6c
sKawashima <bysk.333@gmail.com>
2016-01-18 03:18:24 +0900

    スクロール量に応じた表示の実装

--- js/delay-print.js
+++ js/delay-print.js
@@ -1,16 +1,37 @@
-$(function(){
-	var topView = 800;
-	var place = 700;
-	var work = 1800;
+var $scvalue;
+var topView = 800;
+var place = 700;
+var work = 1800;
 
-	var hide = $('#place .img02');
+var groups = ['.group01' , '.group02', '.group03', '.group04', '.group05', '.group06', '.group07', '.group08', '.group09', '.group10']
+
+$(function(){
+
+	var hide = $('#place .img02, #place .img01, #place .hakobus, .group, #work .img01, #work .hakobus, #movie .img01, #movie .hakobus, #end .hakobus');
+
 
 	hide.css('display','none');
 
 	$(window).scroll(function(){
-		var $scvalue = $(this).scrollTop();//スクロールの値を取得
-		if($scvalue >400){
-			hide.fadeIn(200);
-		}
+		$scvalue = $(this).scrollTop();//スクロールの値を取得
+		sFI('#place .img01',300);
+		sFI('#place .img02',400);
+		sFI('#place .hakobus',300);
+		sFI('#work .img01', topView);
+		sFI('#work .hakobus', topView);
+		$.each(groups,function(i, element){
+			sFI(element,topView + place - 550 + parseInt($(element).css("top")));
+		});
+		sFI('#movie .img01', topView + place + work - 900);
+		sFI('#movie .hakobus', topView + place + work - 900);
+		sFI('#end .hakobus',  topView + place + work - 500);
+
+		sFI('.topButton',topView);
 	});
 });
+
+function sFI(element,value){//scrollFadeIn
+	if($scvalue > value){
+		$(element).fadeIn(500);
+	}
+}
