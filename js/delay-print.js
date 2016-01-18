e022e32ac0df196e9965844bc5410bf2dceca13b
sKawashima <bysk.333@gmail.com>
2016-01-18 11:03:24 +0900

    Merge branch 'master' into sk/mobile
    # Conflicts:
    #	js/delay-print.js

--- js/delay-print.js
+++ js/delay-print.js
@@ -8,9 +8,41 @@ $(function(){
 	hide.css('display','none');
 
 	$(window).scroll(function(){
+<<<<<<< HEAD
 		var $scvalue = $(this).scrollTop();//スクロールの値を取得
 		if($scvalue >400){
 			hide.fadeIn(200);
 		}
 	});
 });
+=======
+		$scvalue = $(this).scrollTop();//スクロールの値を取得
+		sFI('#place .img01',300);
+		sFI('#place .img02',400);
+		sFI('#place .hakobus',300);
+		sFI('#work .img01', topView);
+		sFI('#work .hakobus', topView);
+		$.each(groups,function(i, element){
+			sFI(element,topView + place - 600 + parseInt($(element).css("top")));
+		});
+		sFI('#movie .img01', topView + place + work - 900);
+		sFI('#movie .hakobus', topView + place + work - 900);
+		sFI('#end .hakobus',  topView + place + work - 500);
+
+		sFI('.topButton',topView - 100);
+		sFO('.topButton',topView - 300);
+	});
+});
+
+function sFI(element,value){//scrollFadeIn
+	if($scvalue > value){
+		$(element).fadeIn(500);
+	}
+}
+
+function sFO(element,value){//scrollFadeOut
+	if($scvalue < value){
+		$(element).fadeOut(500);
+	}
+}
+>>>>>>> master
